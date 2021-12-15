c108d9bf24efbc8d3af4630a55f4b73c11d4a03c
yete_wubuntu <yete_wubuntu@yete-windows.localdomain>
2021-12-15 10:31:37 -0300

    adicionando atividade 19

---
+++ modulo-03/atividade-19/script.js
@@ -0,0 +1,130 @@
+const formInput = () => ({
+  nome: document.getElementById("inputName").value.trim(),
+  data: document.getElementById("inputDate").value.replace(/-/g, "/"),
+  valor: document.getElementById("inputValue").value,
+});
+let rowTable = [];
+
+function reverseData(str) {
+  return str.split("/").reverse().join("/");
+}
+
+function addTableForm(el, id){
+    if(id == (rowTable.length - 1)){
+    const tmp = "</td><td>";
+    let tbody = document.getElementById("tBody");
+    tbody.innerHTML = tbody.innerHTML + "<tr><td>" + el.nome + tmp + reverseData(el.data) + tmp + real(Number(el.valor)) + tmp + "" + tmp + "" + tmp + "" + "</td></tr>";
+    }
+}
+
+function addTableJuros(el, id){
+    const tmp = "</td><td>";
+    let tbody = document.getElementById("tBody");
+    tbody.innerHTML = tbody.innerHTML + "<tr><td>" + el.nome + tmp + reverseData(el.data) + tmp + real(Number(el.valor)) + tmp + diasDeAtraso(el.data) + tmp + (jurosPorCento(el.data) * 100).toFixed(1) + "%" + tmp + real(valorTotal(el.data, el.valor)) + "</td></tr>";
+}
+
+function diasDeAtraso(data){
+    const dataAtual = new Date((new Date(Date())).getFullYear(), (new Date(Date())).getMonth(), (new Date(Date())).getDate());
+    const dataCompra = new Date(data);
+    let diasDeAtraso = (dataAtual.getTime() - dataCompra.getTime()) / (1000 * 3600 * 24);
+    
+    if (diasDeAtraso >= 0){
+        return Number(diasDeAtraso.toFixed(0));
+    }
+    else{
+        return "Dentro do Prazo";
+    }
+}
+
+function jurosPorCento(diasAt){
+    if(diasDeAtraso(diasAt) == "Dentro do Prazo"){
+        return 0;
+    }
+    let juros = 0.02 + (0.001 * diasDeAtraso(diasAt));
+    return juros;
+}
+
+function valorTotal(diasAt, valor){
+    let valorTotal = (jurosPorCento(diasAt) * parseFloat(valor)) + parseFloat(valor);
+    return valorTotal;
+}
+
+function real(valor){
+    return valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+
+function adicionar() {
+    if (!document.getElementById("form").checkValidity()){
+        return false;
+    }
+    rowTable.push(formInput());
+    document.getElementById("form").reset()
+    rowTable.map(addTableForm)
+    return rowTable;
+}
+
+function limpar(){
+    rowTable = [];
+    document.getElementById("tBody").innerHTML = " ";
+    return rowTable;
+}
+function calcularJuros(){
+    document.getElementById("tBody").innerHTML = " ";
+    rowTable.map(addTableJuros);
+}
+
+function ordNome(){
+    rowTable.sort(dynamicSort("nome"));
+    document.getElementById("tBody").innerHTML = " ";
+    rowTable.map(addTableJuros);
+}
+
+function ordData(){
+    rowTable.sort(dynamicSort("data"));
+    document.getElementById("tBody").innerHTML = " ";
+    rowTable.map(addTableJuros);
+}
+
+function dynamicSort(property) {
+    let sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+function grupoPor(rowtable, obj) {
+    return rowtable.reduce(function (arr, item) {
+      if (!arr[item[obj]]) {
+        arr[item[obj]] = [];
+      }
+      arr[item[obj]].push(item);
+      return arr;
+    }, {});
+  }
+  
+  /* function ordGrupo(rowtable, obj) {
+    let a = grupoPor(rowtable, obj);
+    a = Object.keys(a)
+      .sort()
+      .reduce(function (acc, key) {
+        acc[key] = a[key];
+        return acc;
+      }, {});
+    a = Object.entries(a).map((e) => ({ [e[0]]: e[1] }));
+    return a;
+  } */
+  
+  function tableGrupo(rowtable, obj) {
+    let grupo = grupoPor(rowtable, obj);
+    let chave = Object.keys(grupo)
+    let valores = [];
+    let soma = chave.map(key =>{
+        valores.push(grupo[key].reduce(function (a, b) {return a.valor + b.valor}));
+    })
+    console.log(soma)
+    console.log(valores)
+  }
