5349fbfd026bc298d475d8691d1955d5e6fe5edf
yete_wubuntu <yete_wubuntu@yete-windows.localdomain>
2022-01-29 08:05:03 -0300

    Atualizando atividade

--- modulo-03/atividade-28/script/index.js
+++ modulo-03/atividade-28/script/index.js
@@ -1,4 +1,78 @@
-import { Calculadora } from "./Calculadora.js";
+//import { Calculadora } from "./Calculadora.js";
+
+class Calculadora {
+  constructor() {
+    this.operand1 = null;
+    this.operand2 = null;
+    this.operation = null;
+  }
+
+  setOperand1(_operand1) {
+    _operand1 = Number(_operand1);
+    if (typeof _operand1 !== "number") {
+      console.error(`The operand 1 (${_operand1}) is not a number.`);
+      return false;
+    }
+    switch (this.operand2) {
+      case null:
+        this.operand1 = _operand1;
+        return true;
+      default:
+        this.operand1 = _operand1;
+        //this.operand1 = this.operand1 * 10 + _operand1;
+        return true;
+    }
+  }
+  setOperand2(_operand2) {
+    _operand2 = Number(_operand2);
+    if (typeof _operand2 !== "number") {
+      console.error(`The operand 2 (${_operand2}) is not a number.`);
+      return false;
+    }
+    switch (this.operand2) {
+      case null:
+        this.operand2 = _operand2;
+        return true;
+      default:
+        this.operand2 = _operand2;
+        //this.operand2 = this.operand2 * 10 + _operand2;
+        return true;
+    }
+  }
+  setOperation(_operation) {
+    let typeOperation = ["+", "-", "*", "/"];
+    if (typeOperation.filter((el) => el === _operation).length !== 1) {
+      console.error(
+        `The ${_operation} operator is wrong, one of these is valid: ${typeOperation}.`
+      );
+      return false;
+    }
+    if (this.operation === null) {
+      this.operation = _operation;
+      return true;
+    }
+  }
+  getResult() {
+    switch (this.operation) {
+      case "+":
+        return this.operand1 + this.operand2;
+      case "-":
+        return this.operand1 - this.operand2;
+      case "/":
+        return this.operand1 / this.operand2;
+      case "*":
+        return this.operand1 * this.operand2;
+      default:
+        return "Error in operator.";
+    }
+  }
+  clearCalculator() {
+    this.operand1 = null;
+    this.operand2 = null;
+    this.operation = null;
+  }
+}
+
 const calc = new Calculadora();
 
 const btnNumber = document.querySelectorAll(".btn.number");
@@ -6,7 +80,6 @@ let count = 0;
 let num1 = "";
 let num2 = "";
 let operation;
-let countResult = 0;
 const display1 = document.getElementById("display1");
 const display2 = document.getElementById("display2");
 
@@ -14,12 +87,12 @@ btnNumber.forEach((el) =>
   el.addEventListener("click", (event) => {
     switch (count) {
       case 0:
-        console.log(event.target.value);
+        //console.log(event.target.value);
         num1 += event.target.value;
         display2.innerHTML = num1;
         break;
       case 1:
-        console.log(event.target.value);
+        //console.log(event.target.value);
         num2 += event.target.value;
         display2.innerHTML = num2;
         break;
